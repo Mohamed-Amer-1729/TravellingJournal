83335f64ac76d22f06181ffc43723849ad635d89
Ahmed Mohsen <am3463485@gmail.com>
2024-10-09 14:01:26 +0300

    Update MyAdaptor.kt

--- app/src/main/java/com/example/travellingjournal/MyAdaptor.kt
+++ app/src/main/java/com/example/travellingjournal/MyAdaptor.kt
@@ -1,86 +1,41 @@
+package com.example.apiapp
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.lifecycle.LiveData
+import androidx.recyclerview.widget.RecyclerView
+import com.example.apiapp.databinding.ItemLayoutBinding
 
-class MyAdapter(
-     private val itemList:MutableList<UserData>,
-     private val onItemDelete:(Pair<UserData,Int>)->Unit,
-     private val showDetails:(UserData)->Unit
-     ):RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class MyAbapter(private val list: LiveData<List<Album>>):RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
+    inner class MyViewHolder( val binding:ItemLayoutBinding):RecyclerView.ViewHolder(binding.root){
+        fun bindData(album: Album){
 
-     /*fun setData(list:MutableList<UserData>){
-         val myDiffUtil:MyDiffUtil= MyDiffUtil(userList,list)
-         val diffResult= DiffUtil.calculateDiff(myDiffUtil)
-         userList=list
-         diffResult.dispatchUpdatesTo(this)
-     }*/
-
-     companion object{
-         const val TYPE_ONE=1
-         const val TYPE_TWO=2
-     }
-
-
-
-     override fun getItemViewType(position: Int): Int {
-         return when(itemList[position].type){
-             1-> TYPE_ONE
-             else-> TYPE_TWO
-         }
-     }
-
-    inner class MyViewHolderOne(val binding: ItemLayoutBinding): RecyclerView.ViewHolder(binding.root) {
-
-        fun bindData(itemData: UserData) {
-            binding.idList.text=itemData.id.toString()
-            binding.userNameList.text=itemData.name
-            binding.emailList.text=itemData.mail
-            binding.phoneList.text=itemData.phone
-            binding.delate.setOnClickListener{
-                onItemDelete(Pair(itemData,adapterPosition))
-            }
-            binding.detailsButton.setOnClickListener{
-
-                showDetails(itemData)
-            }
-
-        }}
-
-     inner class MyViewHolderTwo(val binding:ItemLaoutTwoBinding): RecyclerView.ViewHolder(binding.root) {
-
-        fun bindData(itemData: UserData) {
-            binding.id.text=itemData.id.toString()
-            binding.name.text=itemData.name
-            binding.mail.text=itemData.mail
-            binding.phone.text=itemData.phone
-            binding.delate.setOnClickListener{
-                onItemDelete(Pair(itemData,adapterPosition))
-            }
-        }}
-
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-           when(viewType){
-               TYPE_ONE->{
-                   val binding =
-                       ItemLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-                   return MyViewHolderOne(binding)
-               }
-               else->{
-                   val binding =
-                       ItemLaoutTwoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-                   return MyViewHolderTwo(binding)
-               }
-           }
-        }
-
-        override fun getItemCount() = itemList.size
-        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-            when(holder.itemViewType){
-                TYPE_ONE->(holder as MyViewHolderOne).bindData(itemList[position])
-                else->(holder as MyViewHolderTwo).bindData(itemList[position])
+                binding.apply {
+                    userIdText.text=album.userId.toString()
+                    IdText.text=album.id.toString()
+                    titleText.text=album.title
             }
 
         }
-
 
     }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+val binding=ItemLayoutBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return MyViewHolder(binding)
+    }
+
+    override fun getItemCount()= list.value?.size?:0
+
+    override  fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+
+        val item=list.value!!.get(position)
+        (holder as MyViewHolder).bindData(item)
+
+    }
+}
+
+
+
+
