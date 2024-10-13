8fa4d6bf621c668713fd6885b9283d8660dc5c61
Ahmed <am3463485@gmail.com>
2024-10-13 15:47:28 -0700

    phase two

--- app/src/main/java/com/example/travellingjournal/LocationScreen.kt
+++ app/src/main/java/com/example/travellingjournal/LocationScreen.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.travellingjournal.databinding.LocationScreenBinding
 
 class LocationScreen : Fragment() {
@@ -16,6 +18,19 @@ class LocationScreen : Fragment() {
     ): View? {
        binding=LocationScreenBinding.inflate(layoutInflater,container,false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+
+            notesRecyclerView.layoutManager=LinearLayoutManager(requireContext())
+            addNoteButton.setOnClickListener{
+
+                findNavController().navigate(R.id.addNote)
+            }
+        }
     }
 
 
--- app/src/main/java/com/example/travellingjournal/LocationsScreen.kt
+++ app/src/main/java/com/example/travellingjournal/LocationsScreen.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.travellingjournal.databinding.LocationsScreenBinding
 
 class LocationsScreen : Fragment() {
@@ -21,6 +24,17 @@ class LocationsScreen : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            locationsRecyclerView.layoutManager=LinearLayoutManager(requireContext())
+        addLocationButton.setOnClickListener {
+
+            findNavController().navigate(R.id.addLocation)
+
+        }
+
+        }
+
     }
 
 
